c8149f2959ca8af9d29bc2fd430b4dcf59eb28cd
Ali Midhat <alimidhat62@gmail.com>
2024-04-19 03:59:55 +0200

    Added salting with hash using bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const rounds = 10;
+
+//const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -33,36 +37,52 @@ app.get("/", (req, res) => {
     .post(async(req,res)=>{
         const user = await User.findOne({email:req.body.username});
         if(user){
-            if(user.password === md5(req.body.password)){
+            /*
+            if(user.password === req.body.password){ //compare hash values of user password
                 console.log("Welcome user!");
                 res.render("secrets");
             } else {
                 console.log("Invalid password. Please try again.");
                 res.render("login")
+            }*/
+            bcrypt.compare(req.body.password, user.password, function(err,response){
+                if(response){
+                    console.log("Welcome user!");
+                    res.render("secrets");
+                } else {
+                    console.log("Invalid password. Please try again.");
+                    res.render("login");
+                }
+            });
             }
-        }
+        
         else {
             console.log("No user found with the provided email.");
             res.render("login");
         }
     
-    })
+    });
 
 app.route("/register")
     .get((req, res) => {
     
         res.render("register");
   })
-    .post(async(req,res)=>{
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password) // hash the password and save h
-        });
-        console.log("before hash function=>:" ,req.body.password,"after hash function=>:" ,md5(req.body.password));
-        await newUser.save();
-        console.log("New user registered!");
-        res.render("secrets");
-
+    .post((req,res)=>{
+        const userEmail = req.body.username;
+        const userPassword = req.body.password;
+        bcrypt.hash(userPassword, rounds,async function(err, hash){
+            const newUser = new User({
+                email: userEmail,
+                password: hash // hash the password and save h
+            });
+            console.log("before hash function=>:" ,req.body.password,"after hash function=>:" ,hash);
+            await newUser.save();
+            console.log("New user registered!");
+            res.render("secrets");
+    
+        })
+        
     })
 
 
